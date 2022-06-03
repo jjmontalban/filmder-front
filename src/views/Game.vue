3465d686d1222fe8d71ebfc4c59026faee4f315f
JJMontalban <jjmontalban@gmail.com>
2022-06-03 09:38:21 +0200

    from heroku to github

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -27,42 +27,36 @@
                   <h3>{{ movie.title }}</h3>
                   <br>
                   <span>Number of votes: {{ movie.votes }}</span><br>
-                  <span>which has a average of: {{ movie.vote_avg }}</span><br>
+                  <span>Vote average: {{ movie.vote_avg }}</span><br>
                   
                   <div v-if="movie.user_rating  === 'like'">
-                    You liked this movie and my rating was {{ movie.rating }}<br>
-                    <span class="quote" v-if="movie.rating <= 5">Good movie? Really? Don't know what movie you saw...</span>
-                    <span class="quote" v-else-if="(5 < movie.rating) && (movie.rating < 7)">Movies have to entertain, and this one does...</span>
-                    <span class="quote" v-else>What? There's something you let go. this is better than average...</span>
+                      You liked this movie and my rating was {{ movie.rating }}<br>
+                      <span class="quote" v-if="movie.rating <= 5">Good movie? Really? Don't know what movie you saw...</span>
+                      <span class="quote" v-else-if="(5 < movie.rating) && (movie.rating < 7)">Movies have to entertain, and this one does...</span>
+                      <span class="quote" v-else>What? There's something you let go. this is better than average...</span>
                   </div>
 
                   <div v-else-if="movie.user_rating === 'nope'">
-                    You didnt like and my rating was {{ movie.rating }}<br>  
-                    <span class="quote" v-if="movie.rating <= 5">We agree. It's not a movie to watch again...</span>
-                    <span class="quote" v-else-if="(5 < movie.rating) && (movie.rating < 7)">I recommend that you watch it again...</span>
-                    <span class="quote" v-else>Bad movie? Really??
-                        
-                    </span>
+                        You didnt like and my rating was {{ movie.rating }}<br>  
+                        <span class="quote" v-if="movie.rating <= 5">We agree. It's not a movie to watch again...</span>
+                        <span class="quote" v-else-if="(5 < movie.rating) && (movie.rating < 7)">I recommend that you watch it again...</span>
+                        <span class="quote" v-else>Bad movie? Really??</span>
                   </div>
 
                   <div v-else-if="movie.user_rating === 'super'">
-                    For you this movie is the best and my rating was {{ movie.rating }}<br>
-                    <span v-if="movie.rating <= 5">
-                      <span class="quote" v-if="movie.vote_avg <= 6">Calm down. {{ movie.votes }} people can't be wrong...</span>
-                      <span class="quote" v-else>Meh. It seems you follow the herd...</span>
-                      
-                    </span>
-                    <span class="quote" v-else-if="(5 < movie.rating) && (movie.rating < 7)">I recommend that you watch it again...
-                    </span>
-                    <span class="quote" v-else>We agree. You have good taste
-      
-                    </span>
+                        For you this movie is the best and my rating was {{ movie.rating }}<br>
+                        <span v-if="movie.rating <= 5">
+                            <span class="quote" v-if="movie.vote_avg <= 6">Calm down. {{ movie.votes }} people can't be wrong...</span>
+                            <span class="quote" v-else>Meh. It seems you follow the crowd...</span>
+                        </span>
+                        <span class="quote" v-else-if="(5 < movie.rating) && (movie.rating < 7)">I recommend that you watch it again...</span>
+                        <span class="quote" v-else>We agree. You have good taste</span>
                   </div>
                   <div v-else>
-                    Didn't you see this movie? Let's just let it go...Anyway I scored it with {{ movie.rating }}<br>
-                    <span class="quote" v-if="movie.rating <= 5">You're not missing anything...</span>
-                    <span class="quote" v-else-if="(5 < movie.rating) && (movie.rating < 7)">I recommend that you watch it...</span>
-                    <span class="quote" v-else>Run to see it please!</span>
+                        Didn't you see this movie? Let's just let it go...Anyway I scored it with {{ movie.rating }}<br>
+                        <span class="quote" v-if="movie.rating <= 5">You're not missing anything...</span>
+                        <span class="quote" v-else-if="(5 < movie.rating) && (movie.rating < 7)">I recommend that you watch it...</span>
+                        <span class="quote" v-else>Run to see it please!</span>
                   </div>
                   <br>
                   <hr>
@@ -71,23 +65,28 @@
               </div>
               <h1>Our Film Afinnity</h1>
               <div v-if="movies.total < this.cont/2">
-                  <span class="quote">Sorry but you need to see more films!</span>
+                    <span class="quote">Sorry but you need to see more films!</span>
               </div>
               <div v-else>
-                  <span class="quote" v-if="movies.result < 0">
-                     We don't have the same idea of what cinema is. <a  
-                     ref="https://www.complex.com/pop-culture/2019/10/martin-scorsese-on-marvel-movies">  
-                     Marvel movies is cinema for you?</a>
-                  </span>
-                  <span class="quote" v-else-if="(movies.result > 0) && (movies.result < 3)" >
-                     Not bad. I leave you my references so you can learn <a target="_blank" 
-                     ref="https://www.imdb.com/user/ur22137408/watchlist?sort=your_rating%2Cdesc">
-                     something.</a>
+                    <span class="quote" v-if="movies.result < 0">
+                          We don't have the same idea of what cinema is. 
+                          <a target="_blank" href="https://www.complex.com/pop-culture/2019/10/martin-scorsese-on-marvel-movies">  
+                              Marvel movies is cinema for you?
+                          </a>
                     </span>
-                  <span v-else class="quote">
-                    I like your style. <a target="_blank" ref="https://www.imdb.com/user/ur22137408/watchlist?sort=your_rating%2Cdesc">You can check all my favourite movies for ever.</a> 
-                  </span>
-            </div>
+                    <span class="quote" v-else-if="(movies.result >= 0) && (movies.result < 3)" >
+                          Not bad. I leave you my references so you can learn 
+                          <a target="_blank" href="https://www.imdb.com/user/ur22137408/watchlist?sort=your_rating%2Cdesc">
+                              something.
+                          </a>
+                    </span>
+                    <span v-else class="quote">
+                          I like your style. 
+                          <a target="_blank" href="https://www.imdb.com/user/ur22137408/watchlist?sort=your_rating%2Cdesc">
+                              You can check all my favourite movies for ever.
+                          </a> 
+                    </span>
+              </div>
           </div>
       </transition>
   </v-container>  
@@ -100,9 +99,8 @@ import axios from "axios";
 
 export default {
   
-  name: "Game",
-
-  components: { 'tinder': Tinder },
+    name: "Game",
+    components: { 'tinder': Tinder },
   
     data: () => ({
       // Wordpress Posts Endpoint
@@ -118,11 +116,11 @@ export default {
       ok: true,
     }),
 
-  created() {
-    this.mock();
-  },
+    created() {
+        this.mock();
+    },
 
-  methods: {
+    methods: {
 
         mock() {
           const list = [];
@@ -159,9 +157,9 @@ export default {
             this.movies[this.offset]['user_rating'] = item['type'];
 
             switch(this.movies[this.offset]['user_rating'])
-            {
-                  
+            {      
                   case 'nope':
+
                         if(this.movies[this.offset]['rating'] <= 5){
                             this.movies['result'] = this.movies['result'] + 3;
                             this.movies['total'] = this.movies['total'] + 1;
@@ -176,6 +174,7 @@ export default {
                         break;
                   
                   case 'like':
+
                         if(this.movies[this.offset]['rating'] <= 5){
                             this.movies['result'] = this.movies['result'] - 1;
                             this.movies['total'] = this.movies['total'] + 1;
@@ -191,6 +190,7 @@ export default {
                         break;
 
                   case 'super':
+
                         if(this.movies[this.offset]['rating'] <= 6){
                             this.movies['result'] = this.movies['result'] - 3;
                             this.movies['total'] = this.movies['total'] + 1;
@@ -205,6 +205,7 @@ export default {
                         }
 
             }
+
             console.log("el resultado va siendo", this.movies['result']);
             console.log("y el total de ratings", this.movies['total']);
             this.offset++;
@@ -243,12 +244,9 @@ body {
 
 .tinder-card { 
   border-radius: 0px !important;
-  
 }
 
-
-.nope-pointer,
-.like-pointer {
+.nope-pointer, .like-pointer {
   position: absolute;
   z-index: 1;
   top: 100px;
@@ -286,7 +284,6 @@ body {
   height: 164px;
 }
 
-
 .pic {
   width: 100%;
   height: 100%;
@@ -320,9 +317,11 @@ body {
 .btns img:nth-child(1n) {
   width: 65px;
 }
+
 .btns img:nth-child(2n) {
   width: 53px;
 }
+
 .btns img:nth-child(3n) {
   width: 53px;
 }
@@ -334,7 +333,6 @@ body {
 .btns img:nth-last-child(1) {
   margin-right: 0;
 }
-
 
 /* Transtions */
 .fade-enter-active, .fade-leave-active {
